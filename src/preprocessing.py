2c64c38279178e25b238520a9168f9c0b1fd1ea9
agentmistz <maxtenenbaum223@gmail.com>
2023-12-12 11:53:48 -0800

    Clean up code

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,10 +2,9 @@ import neo
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-#%%
 
-def blkrck_to_numpy(filepath, var_name):
-    reader = neo.io.BlackrockIO(filename=f'filepath')
+def blkrck_to_numpy(filepath):
+    reader = neo.io.BlackrockIO(filename=f'{filepath}')
     blk = reader.read_block()
     print("Number of segments:", len(blk.segments))
     
@@ -14,16 +13,20 @@ def blkrck_to_numpy(filepath, var_name):
         print("  Number of analog signals:", len(seg.analogsignals))
         print("  Number of spike trains:", len(seg.spiketrains))
         print("  Number of events:", len(seg.events))
+        
     if len(blk.segments) > 0 and len(blk.segments[0].analogsignals) > 0:
         analog_signal = blk.segments[0].analogsignals[0]
         print(analog_signal)
-    # Optionally, convert to a NumPy array and print the shape
-    var_name = analog_signal.magnitude
-    print('Array shape =',var_name.shape)
-    print('Max =',var_name.max(), '// Min =', var_name.min(), '// Mean =', var_name.mean())
-    return var_name
+        
+        # Convert to a NumPy array
+        data_array = analog_signal.magnitude
+        print('Array shape =', data_array.shape)
+        print('Max =', data_array.max(), '// Min =', data_array.min(), '// Mean =', data_array.mean())
+        
+        return data_array
+    else:
+        return None
 
-#%%
-
-blkrck_to_numpy("/Users/maxtenenbaum/Desktop/Deku Ephys/deku_ephys/data/raw/2023_11_20 RefScrewLocation1001.ns4", "test")
-# %%
+# Example usage
+data = blkrck_to_numpy("data/raw/2023_11_20 RefScrewLocation1001.ns6")
+print(data)
