94e7623612ad743bd4413acb71f0db45990fafe8
agentmistz <maxtenenbaum223@gmail.com>
2023-12-14 22:27:32 -0800

    Edit init

--- src/__init__.py
+++ src/__init__.py
@@ -3,10 +3,10 @@ from .analysis import plot_stft, power_spectral_density
 from .visualization import plot_data
 
 __all__ = [
-    'blkrck_to_numpy'
-    'butter_bandpass_filter'
-    'filter_outliers'
-    'plot_stft'
-    'power_spectral_density'
+    'blkrck_to_numpy',
+    'butter_bandpass_filter',
+    'filter_outliers',
+    'plot_stft',
+    'power_spectral_density',
     'plot_data'
 ]
--- tests/pipeline.py
+++ tests/pipeline.py
@@ -1,1 +1,4 @@
-from prepr
+from src import blkrck_to_numpy
+
+
+europa_data = blkrck_to_numpy('data/raw/121323T1E1_Europa16_E6_30kHz003.ns5')
